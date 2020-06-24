edca760ef758a9a262bdcad13c10ec046ba16bb7
bangmac <anhbexd@gmail.com>
2020-06-24 15:53:26 +0700

    all of object init

---
+++ allOfObject/src/main/java/com/codegym/cms/model/Province.java
@@ -0,0 +1,42 @@
+package com.codegym.cms.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "provinces")
+public class Province {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    public Province(){}
+
+    @OneToMany(targetEntity = Customer.class)
+    private List<Customer> customers;
+
+    public List<Customer> getCustomers(){
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers){
+        this.customers = customers;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
